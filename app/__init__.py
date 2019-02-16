be4ab1f47dd598448b8e00608551576036ac86af
Geerocktricks <geerockface4@gmail.com>
2019-02-16 13:06:09 +0300

    This commit styles the index page and installs flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
 
 #initializing the app
 app = Flask(__name__)
 
 #setting up configs
 app.config.from_object(DevConfig)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
