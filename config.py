ff4b8c5ae548abf4f5cececdec043fb0d3d5a5dc
Geerocktricks <geerockface4@gmail.com>
2019-02-17 01:51:16 +0300

    This commit registers the app Blueprint

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 class Config:
     '''
     General configuration parent class
@@ -17,11 +18,9 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
