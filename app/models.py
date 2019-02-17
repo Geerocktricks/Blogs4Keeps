016da3cd31d1bb6092c01b21409b01e8b4d3f643
Geerocktricks <geerockface4@gmail.com>
2019-02-17 15:14:53 +0300

    This commit creates the Blog class that creates blog instances

--- app/models.py
+++ app/models.py
@@ -19,6 +19,7 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    blogs = db.relationship("Blog", backref= "user", lazy="dynamic")
     pass_secure = db.Column(db.String(255))
 
     @property
@@ -48,3 +49,28 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class Blog(db.Model):
+    """
+    This is the class which we will use to create the blogs for the app
+    """
+    __tablename__ = "blogs"
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    category = db.Column(db.String)
+    date = db.Column(db.String)
+    time = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # comments = db.relationship("Comment", backref = "pitch", lazy = "dynamic")
+
+
+    def save_blogs(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_blogs(cls,id):
+        blog = Blog.query.filter_by(blog_id = blog.id).order_by(blog.time.desc())
+        return reviews
