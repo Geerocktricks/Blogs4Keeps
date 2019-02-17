ae9348dcc045b1daa04602939d85137e647d1ceb
Geerocktricks <geerockface4@gmail.com>
2019-02-17 16:27:30 +0300

    This commit creates a test to see if news blogs are saved

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -12,10 +12,14 @@ def tearDown(self):
 
 
 def test_check_instance_variables(self):
-        self.assertEquals(self.new_review.id,12345)
-        self.assertEquals(self.new_review.title,'Full stack development')
-        self.assertEquals(self.new_review.content,"Is it safe to go the full-stack way or better the Android path")
-        self.assertEquals(self.new_review.category,'technology')
-        self.assertEquals(self.new_review.date, 1-12-2019)
-        self.assertEquals(self.new_review. time,16:04)
-        self.assertEquals(self.new_review.user,self.user_Gerald)
+        self.assertEquals(self.new_blog.id,12345)
+        self.assertEquals(self.new_blog.title,'Full stack development')
+        self.assertEquals(self.new_blog.content,"Is it safe to go the full-stack way or better the Android path")
+        self.assertEquals(self.new_blog.category,'technology')
+        self.assertEquals(self.new_blog.date, 1-12-2019)
+        self.assertEquals(self.new_blog. time,16:04)
+        self.assertEquals(self.new_blog.user,self.user_Gerald)
+
+def test_save_blog(self):
+        self.new_blog.save_blog()
+        self.assertTrue(len(Review.query.all())>0)
