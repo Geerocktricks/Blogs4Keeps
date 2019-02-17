38f4067ceaa52092c66d61390166153c3799c2f2
Geerocktricks <geerockface4@gmail.com>
2019-02-17 10:33:51 +0300

    This commit creates the profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
-from flask import render_template
-# from app import app
+from flask import render_template,request,redirect,url_for,abort
 from . import main
-from flask_login import login_required
+from flask_login import login_required, current_user
+from ..models import User, Role
+from .. import db
+import datetime
 
 #views
 @main.route('/')
@@ -11,3 +13,16 @@ def index():
     '''
     title = 'My personal Blog'
     return render_template('index.html' , title = title)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    '''
+    function that returns the profile page
+    '''
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
