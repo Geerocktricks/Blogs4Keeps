c2a090bc7edc27738c1a0bdd56759bb7995cf80c
Geerocktricks <geerockface4@gmail.com>
2019-02-17 03:25:31 +0300

    This commit creates the Roles model and the conection to the user model

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://geerocktricks:Geerock_1@localhost/blogs'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 
